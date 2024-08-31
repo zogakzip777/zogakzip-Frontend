b057aae4394e96164c178005b03a9ca076763d4b
laura-jung <jungya@ewhain.net>
2024-08-31 19:12:13 +0900

    로고변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import GroupListPage from './GroupList/GroupListPage';
-import GroupDetailPage from './GroupDetailPage'; // 그룹 상세 페이지 컴포넌트
-import GroupPage from './그룹상세조회/GroupPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GroupListPage from "./GroupList/GroupListPage";
+import GroupPage from "./그룹상세조회/GroupPage";
+import MemoryPage from "./그룹상세조회/MemoryPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<GroupListPage />} />
-        <Route path="/group/:id" element={<GroupPage />} />  {/* 그룹 상세 페이지 */}
+        <Route path="/group/:id" element={<GroupPage />} />{" "}
+        {/* 그룹 상세 페이지 */}
+        <Route path="/memory/:groupId" element={<MemoryPage />} />{" "}
+        {/* 추억게시물 */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
