6109fee36c591a58256ce79517e171c454f5ec2f
seyeon <131129047+leelinobuin@users.noreply.github.com>
2024-08-31 20:46:13 +0900

    Appjs

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GroupListPage from "./GroupList/GroupListPage";
 import GroupPage from "./그룹상세조회/GroupPage";
 import MemoryPage from "./그룹상세조회/MemoryPage";
+import PostDetail from "./PostDetail/PostDetail"; // 새로 추가
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<GroupListPage />} />
-        <Route path="/group/:id" element={<GroupPage />} />{" "}
-        {/* 그룹 상세 페이지 */}
-        <Route path="/memory/:groupId" element={<MemoryPage />} />{" "}
-        {/* 추억게시물 */}
+        <Route path="/group/:id" element={<GroupPage />} />
+        <Route path="/memory/:groupId" element={<MemoryPage />} />
+        <Route path="/post/:postId" element={<PostDetail />} /> {/* 새로 추가한 라우트 */}
       </Routes>
     </Router>
   );
